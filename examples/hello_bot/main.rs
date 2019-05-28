770363f29f1e87ef583ffa23804c7c812e02bcaf
Matt O'Tousa <matt@mattotousa.com>
2019-05-27 21:48:31 -0700

    updated example

--- examples/hello_bot/main.rs
+++ examples/hello_bot/main.rs
@@ -9,32 +9,33 @@ use log::LevelFilter;
 use rocket_bot::{
     RocketBot,
     User,
-    MessageHandler
+    RocketMessageHandler
 };
 //use env_logger;
 
 
-const DOMAIN: &str = "rocket.cat.pdx.edu/websocket";
+const DOMAIN: &str = "wss://rocket.cat.pdx.edu/websocket";
 // const DOMAIN: &str = "echo.websocket.org";
 const USERNAME: &str = "rustbot";
 const PASSWORD: &str = "zVd/HkU9djMBhloZCwaXf4PveNyP56";
 
 struct HelloHandler {}
 
-impl MessageHandler for HelloHandler {
-    fn on_message_recieved(self) -> Result<i32, &'static str> {
-        Ok(1)
+impl RocketMessageHandler for HelloHandler {
+    fn on_message(self) {
+        ();
     }
 }
 
 
 fn main() -> Result<(), ()> {
     simple_logging::log_to_stderr(LevelFilter::Info);
-    let hello_bot = HelloHandler {};
+    let _hello_bot = HelloHandler {};
     let user = User::new(
         String::from(USERNAME),
         String::from(PASSWORD)
     );
+    /*
     let mut rocket_bot = RocketBot::new(
         String::from(DOMAIN),
         user,
@@ -43,6 +44,8 @@ fn main() -> Result<(), ()> {
     match rocket_bot.run() {
         _ => {}
     }
+    */
+    RocketBot::run(DOMAIN.to_string(), user);
 
     Ok(())
 }
